315aef8762d5cdfe89b50e172c24c8a6cb4aa413
HamjaaLeo <115864823+HamjaaLeo@users.noreply.github.com>
2022-11-09 12:01:10 +0530

    Add files via upload

---
+++ iBus/src/main/java/com/masai/controllers/ReservationController.java
@@ -0,0 +1,17 @@
+package com.masai.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationController {
+	
+//	@Autowired
+////	private ReservationService rs;
+//	
+	
+
+	
+	
+	
+}
